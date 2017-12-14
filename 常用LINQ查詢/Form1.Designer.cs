7d56f0343a8dfdb159a55b0858d82acdb30b8398
joelee2017 <as955133@gmail.com>
2017-12-14 11:44:05 +0800

    上傳

--- 常用LINQ查詢/Form1.Designer.cs
+++ 常用LINQ查詢/Form1.Designer.cs
@@ -28,12 +28,134 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCompanyName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCountry = new System.Windows.Forms.ComboBox();
+            this.btnQuery = new System.Windows.Forms.Button();
+            this.cbOrders = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.labtotal = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "客戶";
+            // 
+            // btnCompanyName
+            // 
+            this.btnCompanyName.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.btnCompanyName.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.btnCompanyName.Location = new System.Drawing.Point(79, 25);
+            this.btnCompanyName.Name = "btnCompanyName";
+            this.btnCompanyName.Size = new System.Drawing.Size(224, 22);
+            this.btnCompanyName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(455, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "國家";
+            // 
+            // btnCountry
+            // 
+            this.btnCountry.FormattingEnabled = true;
+            this.btnCountry.Location = new System.Drawing.Point(507, 25);
+            this.btnCountry.Name = "btnCountry";
+            this.btnCountry.Size = new System.Drawing.Size(136, 20);
+            this.btnCountry.TabIndex = 3;
+            // 
+            // btnQuery
+            // 
+            this.btnQuery.Location = new System.Drawing.Point(322, 25);
+            this.btnQuery.Name = "btnQuery";
+            this.btnQuery.Size = new System.Drawing.Size(75, 23);
+            this.btnQuery.TabIndex = 4;
+            this.btnQuery.Text = "查詢";
+            this.btnQuery.UseVisualStyleBackColor = true;
+            this.btnQuery.Click += new System.EventHandler(this.btnQuery_Click);
+            // 
+            // cbOrders
+            // 
+            this.cbOrders.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbOrders.FormattingEnabled = true;
+            this.cbOrders.Location = new System.Drawing.Point(79, 80);
+            this.cbOrders.Name = "cbOrders";
+            this.cbOrders.Size = new System.Drawing.Size(224, 20);
+            this.cbOrders.TabIndex = 5;
+            this.cbOrders.SelectedIndexChanged += new System.EventHandler(this.cbOrders_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "訂單";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(28, 195);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(615, 145);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // labtotal
+            // 
+            this.labtotal.AutoSize = true;
+            this.labtotal.Location = new System.Drawing.Point(520, 358);
+            this.labtotal.Name = "labtotal";
+            this.labtotal.Size = new System.Drawing.Size(29, 12);
+            this.labtotal.TabIndex = 8;
+            this.labtotal.Text = "總計";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(655, 507);
+            this.Controls.Add(this.labtotal);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cbOrders);
+            this.Controls.Add(this.btnQuery);
+            this.Controls.Add(this.btnCountry);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnCompanyName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox btnCompanyName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox btnCountry;
+        private System.Windows.Forms.Button btnQuery;
+        private System.Windows.Forms.ComboBox cbOrders;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        internal System.Windows.Forms.Label labtotal;
     }
 }
 
--- 常用LINQ查詢/Form1.cs
+++ 常用LINQ查詢/Form1.cs
@@ -16,5 +16,75 @@ namespace 常用LINQ查詢
         {
             InitializeComponent();
         }
+
+        /// <summary>
+        /// 建立 AutoCompleteStringCollection 自動填入值(提示)
+        /// asc.AddRange項目加入結尾，將CompanyName加入
+        /// btnCompanyName.AutoCompleteCustomSource = asc;
+        /// 控制項btnCompanyName使用AutoCompleteCustomSource屬性，實作asc
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)//在Textbox控制項，出現提示
+        {
+            NorthwindEntities dc = new NorthwindEntities();//Entity連結db
+            AutoCompleteStringCollection asc = new AutoCompleteStringCollection();
+            asc.AddRange(dc.Customers.Select(c => c.CompanyName).ToArray());
+            btnCompanyName.AutoCompleteCustomSource = asc;
+        }
+
+        /// <summary>
+        /// NorthwindEntities dc = new NorthwindEntities();因快取機制建議每次都新建一個
+        /// query 查詢==textbox中的值，Single只取得一筆記錄 查詢ordersID
+        /// 錯誤訊息 if (combobox 資料來源 query.toarray) else 查詢 null 的話顯示尚無訂單
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnQuery_Click(object sender, EventArgs e)
+        {
+            NorthwindEntities dc = new NorthwindEntities();
+            var query = dc.Customers.Where(c => c.CompanyName == btnCompanyName.Text)
+                                                .Single().Orders.Select(o => o.OrderID);
+            if(query.Count() !=0)
+            {
+                cbOrders.DataSource = query.ToArray();
+            }
+            else
+            {
+                cbOrders.DataSource = null;
+                MessageBox.Show("尚無訂單");
+            }            
+
+        }
+
+        /// <summary>
+        /// dc.Order_Details.ToArray() 條件後先將欲查詢目標資料表ToArray() 轉成集合
+        /// int.Parse( cbOrders.Text)因型態不同，使用parse
+        /// od.UnitPrice * od.Quantity*(1 -(decimal)od.Discount) 型態不同所以轉型(decimal)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void cbOrders_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            NorthwindEntities dc = new NorthwindEntities();
+            var query = dc.Order_Details.ToArray().Where
+                            (od => od.OrderID == int.Parse( cbOrders.Text));
+            
+            dataGridView1.DataSource = query.Select(od => new
+            {
+
+                訂單編號 = od.OrderID,
+                商品編號 = od.ProductID,
+                商品單價 = od.UnitPrice,
+                購買數量 = od.Quantity,
+                優惠折扣 = od.Discount,
+                商品小計 = (od.UnitPrice * od.Quantity * (1 - (decimal)od.Discount)).ToString("c")
+            }).ToArray();
+
+            decimal subtotal = query.Sum(od =>
+                                    od.UnitPrice * od.Quantity*(1 -(decimal)od.Discount));
+            labtotal.Text = string.Format($"小計：{subtotal.ToString("C")}");
+
+        }
     }
 }
